15f099797e002453c3e4eca11c22f0a8d3c49696
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2025-12-07 16:31:40 +0100

    Add Mongo database

---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Global, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Global()
+@Module({
+    imports: [
+        ConfigModule,
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                const user = config.get<string>('MONGODB_USER');
+                const pwd = config.get<string>('MONGODB_PWD');
+                const host = config.get<string>('MONGODB_HOST');
+
+                return {
+                    uri: 'mongodb://' + user + ':' + pwd + '@' + host + '/recipes?authSource=recipes',
+                    retryAttempts: 5,
+                    retryDelay: 5000,
+                };
+            },
+        }),
+    ],
+    exports: [MongooseModule],
+})
+export class DatabaseModule {}
