38fd543dbf13dacb6db832ac69140aa4581ff36a
Eric Le Guellaut <eric.le.guellaut@hotmail.fr>
2026-04-26 16:21:46 +0200

    Add security management with Helmet and CORS

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import helmet from 'helmet'
 import { join } from 'path'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+    app.use(helmet())
+    app.enableCors({
+        origin: false,
+        credentials: true,
+    })
 
     app.setGlobalPrefix('api')
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
